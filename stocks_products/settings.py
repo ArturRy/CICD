78f9a39296867e0598b4179fa5220b141cf351bc
Artur <mini--me@mail.ru>
2024-01-26 21:38:44 +0300

    m

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['89.111.154.186']
 
 
 # Application definition
